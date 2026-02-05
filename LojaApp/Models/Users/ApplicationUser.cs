0af42e04c5e1269b6219f648a6d57465c5ec6b83
michelgarciafurtado <michelgarciafurtado@gmail.com>
2026-02-05 07:31:59 -0300

    testando multiplos targets

--- LojaApp/Models/Users/ApplicationUser.cs
+++ LojaApp/Models/Users/ApplicationUser.cs
@@ -14,3 +14,22 @@ public class ApplicationUser:IdentityUser
     
     }
 }
+
+public record RegisterViewModel { 
+    [Required]
+    [Display(Name = "Nome completo")] 
+    public string NomeCompleto { get; set; }
+    [Required]
+    [Display(Name = "CPF")]
+    public string CPF { get; set; } 
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirmar senha")]
+    [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+    public string ConfirmPassword { get; set; }
+}
